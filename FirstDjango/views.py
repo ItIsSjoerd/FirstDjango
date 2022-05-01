caecca02febe5aee021f5a2bcfe68337e8c98d76
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 00:58:48 +0200

    First View

--- FirstDjango/views.py
+++ FirstDjango/views.py
@@ -1,25 +1,37 @@
 """
 To Render HTML Web Pages.
 """
+from multiprocessing import context
 from django.http import HttpResponse
-
-def name_print(name):
+from articles.models import Article
     
-    HTML_STRING = f"""
-    <h1>Hello, World!</h1>
-    <span>Hallo {name}, dit is een test.</span>
-    """
+def name_printer(name):
+
+
+    article_obj = Article.objects.get(id=3)
+
+    
+    context = {
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "content": article_obj.content,
+        "name": name,
+    }
+
+    HTML_STRING = """
+    <h1>{title} (ID: {id})</h1>
+    <span>Hallo {name}, {content}.</span>
+    """.format(**context)
 
     return HTML_STRING
 
 
 def home_view(request):
-
     """
     Take in request.(Django sends request)
     Send HTML as response.
     """
 
-    HTML_STRING = name_print('sjoerd') + name_print('sieger')
+    HTML_STRING = name_printer('sjoerd') + name_printer('sieger')
 
     return HttpResponse(HTML_STRING)
